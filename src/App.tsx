a40d71e83ca7f6d2bf836d93d9b6aa5cde17db30
xB0t <ychaudhari124@gmail.com>
2023-11-25 22:18:15 +0530

    Added 404.html in dist for Github React Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,8 @@
 import './App.css'
 
-// function App() {
-
-//   return (
-//     <>
-//     <h1>Hi, I am Yash</h1>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-// App.js
-// Kindacode.com
-
-// import { useEffect, useState } from "react";
-
-// const App = () => {
-//   const [title, setTitle] = useState("Portfolio");
-
-//   useEffect(() => {
-//     // This will run when the page first loads and whenever the title changes
-//     document.title = title;
-//   }, [title]);
-
-//   const changeTitle = (event) => {
-//     setTitle(event.target.value);
-//   };
-
-
-//   let title2 = "Home Page"
-
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         onChange={changeTitle}
-//         value={title}
-//       />
-
-    
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-
 import { useEffect, useState } from "react";
 
 const App = () => {
-  // Set the default title to "Portfolio"
   const [title] = useState("xB0t Portfolio");
 
   let page = "Home";
