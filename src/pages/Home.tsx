3717166176e519c53624da3d9e7e165b7da9266c
xB0t <ychaudhari124@gmail.com>
2023-11-26 22:09:59 +0530

    Added Tailwind CSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,23 @@ import { Link } from 'react-router-dom';
 import Title from '../components/Title';
 
 
+
 const Home = () => {
 
   return (
     <>
       <Title title="Home Page" />
 
-      <h1>Hi, I am Yash</h1>
+      <img className="rounded-full w-40 h-40" src="src/assets/react.svg" alt="image description" />
+      
+      <h1 className="text-6xl font-bold intro">Hi, I am Yash</h1>
+
+      <h2> I am Cloud Engineer, Backend Web Developer.</h2>
+
+
+      <br />
+      <br />
+      <br />
 
       <Link to='/about'> About</Link>
     </>
