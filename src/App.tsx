c64836cfa652118fac5865e4f97bfc94fe268b29
xB0t <ychaudhari124@gmail.com>
2023-11-27 21:53:59 +0530

    Improved App.tsx & App.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import Home from './pages/Home';
-import About from './pages/About';
+import Projects from './pages/Projects';
 import PageNotFound from './pages/PageNotFound';
+import Footer from './components/Footer';
 
 const App = () => {
 
@@ -13,11 +14,12 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
 
+      <Footer />
     </>
 
   );
