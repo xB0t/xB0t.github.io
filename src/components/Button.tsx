f383ff42d6ee93731e72a956924bb6c49ea453db
xB0t <ychaudhari124@gmail.com>
2023-12-12 00:29:57 +0530

    Added Button Component for resuability

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+const Button = ({ name, btnDivStyling, btnStyling, navigateTo }: any) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={btnDivStyling}>
+        <button
+          type="button"
+          className={btnStyling}
+          onClick={() => navigate(`${navigateTo}`)}
+        >
+          {name}
+        </button>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Button;
