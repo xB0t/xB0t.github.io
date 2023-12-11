b66ef301705e447511c5f0a1813a894f87097d17
xB0t <ychaudhari124@gmail.com>
2023-12-12 00:31:05 +0530

    Added Button to Projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,18 @@
-import { Link } from 'react-router-dom';
 import Title from "../components/Title";
+import Button from "../components/Button";
 
 const About = () => {
-
   return (
-    <div>
+    <>
       <Title title="Projects" />
       <h1>Projects Page</h1>
-      <Link to='/'> Home</Link>
-    </div>
+      <Button
+        name="Home"
+        btnDivStyling="grid place-items-center"
+        btnStyling="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2"
+        navigateTo="/"
+      />
+    </>
   );
 };
 
