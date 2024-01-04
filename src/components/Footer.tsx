bc9a76a2a4859915c00a7160b3f209b279d94477
xB0t <ychaudhari124@gmail.com>
2024-01-04 22:27:16 +0530

    Updated Home Page & Footer Component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,16 @@
 const Footer = () => {
   return (
     <>
-      <div className="grid place-items-center">
-        <footer className="fixed center-0 bottom-0 pb-10">
+      <div className="grid place-items-center max-sm:pb-6">
+        <footer className="block center-0 bottom-0 pt-28 max-sm:pt-2">
           {/* <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"> */}
           <span className="max-sm:text-white max-sm:pl-4 text-white sm:text-center dark:text-gray-400 text-xl">
             © 2023{" "}
-            <a href="https://github.com/xb0t" className="hover:underline">
+            <a
+              target="_blank"
+              href="https://github.com/xb0t"
+              className="hover:underline"
+            >
               xB0t
             </a>
             . All Rights Reserved.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ const Home = () => {
       <Tools />
       <Button
         name="Projects"
-        btnDivStyling="grid place-items-center max-sm:pb-28"
+        btnDivStyling="grid place-items-center pt-10 max-sm:pb-10"
         btnStyling="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2"
         navigateTo="/projects"
       />
