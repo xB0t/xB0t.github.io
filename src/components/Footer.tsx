b946747e177e0e5ea612985aea72d9cca85727b9
xB0t <ychaudhari124@gmail.com>
2024-01-04 22:49:55 +0530

    Updated Footer for automatic year changes.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
+  let date = new Date().getFullYear();
+
   return (
     <>
       <div className="grid place-items-center max-sm:pb-6">
         <footer className="block center-0 bottom-0 pt-28 max-sm:pt-2">
           {/* <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"> */}
           <span className="max-sm:text-white max-sm:pl-4 text-white sm:text-center dark:text-gray-400 text-xl">
-            © 2023{" "}
+            © {date}{" "}
             <a
               target="_blank"
               href="https://github.com/xb0t"
