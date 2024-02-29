6a44acf680893b34f36fba66ab3b633d98ffd914
xB0t <ychaudhari124@gmail.com>
2024-02-29 16:21:06 +0530

    Added Blog Page

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../components/Button";
+
+const Blog = () => {
+  return (
+    <>
+      <Button
+        name="Home"
+        btnDivStyling="grid place-items-center"
+        btnStyling="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2"
+        navigateTo="/"
+      />
+      All My WordPress Blogs
+    </>
+  );
+};
+
+export default Blog;
