4107af6d5c8481c9c572e1cb2878c684fe8d8c14
xB0t <ychaudhari124@gmail.com>
2024-02-29 16:23:01 +0530

    Updated Button for navigating outside urls

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,21 @@ import { useNavigate } from "react-router-dom";
 
 const Button = ({ name, btnDivStyling, btnStyling, navigateTo }: any) => {
   const navigate = useNavigate();
+
+  const externalUrl = (url: string) =>
+    url.startsWith("http://") || url.startsWith("https://");
+
+  const navigateUrl = (url: string) => {
+    externalUrl(url) ? window.open(url, "_blank") : navigate(url);
+  };
+
   return (
     <>
       <div className={btnDivStyling}>
         <button
           type="button"
           className={btnStyling}
-          onClick={() => navigate(`${navigateTo}`)}
+          onClick={() => navigateUrl(`${navigateTo}`)}
         >
           {name}
         </button>
